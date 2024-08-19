f0fec20df17b16c50e2062c37887d23e11a6d271
Joshuan <jtruda25@gmail.com>
2024-08-19 22:15:22 +0200

    added flask

--- tokenrequest.py
+++ tokenrequest.py
@@ -1,10 +1,12 @@
+from flask import Flask, jsonify
 import requests
 import base64
 
 CLIENT_ID = 'ae596fd96e6c460a9613c41c6e1fccce'
 CLIENT_SECRET = '8ee9b071fb1c457d91ba8e326348abf4'
-REDIRECT_URI = 'https://alecchen.dev/spotify-refresh-token'
 REFRESH_TOKEN = 'AQDa-IZZW8HcFyNK3gD5gGidMZ0PbiZipKHYedthxgKisDd1k8E0ynZ748ZC6D4Er_EVEuFw_bbs07VgvDy0ngI2Pus5N8tavc_3N-tcDuLeIn7GHV0WkAQNjzVh_pjv0Eg'
+
+app = Flask(__name__)
 
 def refresh_access_token():
     url = 'https://accounts.spotify.com/api/token'
@@ -19,6 +21,10 @@ def refresh_access_token():
     response = requests.post(url, headers=headers, data=data)
     return response.json()['access_token']
 
-if __name__ == "__main__":
+@app.route('/token', methods=['GET'])
+def get_token():
     access_token = refresh_access_token()
-    print(f"Access Token: {access_token}")
+    return jsonify({"access_token": access_token})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
